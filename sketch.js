35db723b00dfa15c04a7fa12ab39f783726ce314
joeythornberry <lewis.thornberry@christendom.edu>
2024-02-19 18:37:57 -0500

    [feature] added crusader sprite

--- sketch.js
+++ sketch.js
@@ -10,6 +10,63 @@ function drawBasicCryptid(x,y,s,a) {
 	pop()
 }
 
+function drawCrusader(x,y,s,a) {
+	push();
+	translate(x,y)
+	scale(s)
+	rotate(a)
+	
+	// horse
+	fill(0)
+	stroke(0)
+	ellipse(0,20,50,25)
+
+	// horse neck
+	strokeWeight(8)
+	line(20,10,30,0)
+	strokeWeight(1)
+
+	// horse head
+	triangle(25,0,50,0,25,-12)
+
+	// horse legs
+	strokeWeight(5)
+	line(15,15,40,40)
+	line(15,15,30,40)
+	line(-10,15,-40,40)
+	line(-10,15,-30,40)
+	// tail
+	line(-20,20,-30,0)
+	strokeWeight(1)
+
+
+	// armor
+	stroke(0,0,0)
+	fill(200,200,200)
+	ellipse(0,0,10,30)
+	triangle(-2,-10,7,-10,-2,-20)
+
+	push()
+	translate(6,20)
+	rotate(-PI/6)
+	ellipse(0,0,10,20)
+	pop()
+
+	// crest
+	stroke(255,0,0)
+	strokeWeight(5)
+	noFill()
+	arc(-2,-15,20,20,PI,-PI/6)
+
+	// lance
+	noStroke()
+	fill(0,0,255)
+	rect(-20,2.5,20,5)
+	triangle(0,10,80,5,0,0)
+
+	pop()
+}
+
 
 /*** this is where the code that draws your cryptid goes ***/
 function drawGooby(x,y,s,a) {
@@ -99,7 +156,7 @@ function drawGooby(x,y,s,a) {
 
 /*** this code is called once, at the start of the program ***/
 function setup() {
-	createCanvas(1000, 1000);
+	createCanvas(700, 700);
 	background(255,255,255);
 }
 
@@ -111,8 +168,20 @@ let gooby_y = 200;
 
 let visible = true;
 
+let crusader_x = 400;
+let crusader_y = 200;
+
 /*** this code is called 60 times per second ***/
 function draw() {
+	handleKeyPress()
+
+	let distance = ((crusader_x - gooby_x)**2 + (crusader_y - gooby_y)**2)**0.5
+
+	if (distance < 100) {
+		// visible = !visible;
+		visible = false
+	}
+
 	background(255,255,255);
 
 	if (visible) {
@@ -128,7 +197,10 @@ function draw() {
 
 	if (gooby_x > 1000) {
 		gooby_x = -50;
+		visible = true;
 	}
+
+	drawCrusader(crusader_x,crusader_y,1,0)
 }
 
 function mouseClicked() {
@@ -136,5 +208,19 @@ function mouseClicked() {
 
 	if (distance < 100) {
 		visible = !visible;
+	}
+}
+
+function handleKeyPress() {
+	if (keyIsPressed) {
+		if (keyCode === UP_ARROW) {
+			crusader_y -= 10;
+		} else if (keyCode === DOWN_ARROW) {
+			crusader_y += 10;
+		} else if (keyCode === LEFT_ARROW) {
+			crusader_x -= 10;
+		} else if (keyCode === RIGHT_ARROW) {
+			crusader_x += 10;
+		}
 	}
 }
